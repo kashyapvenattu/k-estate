d73b128472590c0778b275104e312e1963619854
Kashyap <kashyapvenattu@gmail.com>
2024-05-05 14:03:16 -0600

    basic error handling using try catch for signup

--- server/controller/auth-controller.js
+++ server/controller/auth-controller.js
@@ -5,6 +5,11 @@ export const postSignUp = async (req, res) => {
     const {username, email, password} = req.body
     const hashPass = bcryptjs.hashSync(password, 10)
     const newUser = new User({username, email, password: hashPass})
-    await newUser.save()
-    res.status(201).json('User Created Successfully!!!')
+    try {
+        await newUser.save()
+        res.status(201).json('User Created Successfully!!!')        
+    } catch (error) {
+        res.status(500).json(error.message)
+    }
+
 }
